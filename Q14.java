e050acb10f3948fd539f05846e583e645d5c1ce2
Shivashish Das <dasshivashish11@gmail.com>
2022-06-08 14:22:40 +0530

    Fixed file encondings and Q14 incomplete

---
+++ Q14.java
@@ -0,0 +1,17 @@
+/* 14. Define a class named Fruit Juice with the following description:
+Instance variables / data members :
+int product_code - stores the product code number
+String flavour - stores the flavour of the juice (E.g. orange, apple, etc.)
+String pack_type - stores the type of packaging (E.g. tetra-pack, PET bottle, etc.)
+int pack_size - stores package size (E.g. 200 ml, 400 ml, etc.)
+int product_price - stores the price of the product 
+
+Member methods :
+(i) FruitJuice() - Default constructor to initialize integer data members to 0 and
+String data members to ""
+(ii) void input( ) - To input and store the product code, flavour, pack type, pack
+size and product price.
+(iii) void discount( ) - To reduce the product price by 10.
+(iv) void display() -  To display the product code, flavour, pack type, pack size
+and product price.
+*/
